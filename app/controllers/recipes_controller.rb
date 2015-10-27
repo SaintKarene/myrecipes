2f8075897c027449c7e209c84c7377eaaaa61477
SaintKarene <stkarene@gmail.com>
2015-10-27 21:00:06 +0000

    added image uploader

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -30,7 +30,7 @@ class RecipesController < ApplicationController
   end
   private
       def recipe_params
-        params.require(:recipe).permit(:name, :summary, :description)
+        params.require(:recipe).permit(:name, :summary, :description, :picture)
       end
   
   def edit
