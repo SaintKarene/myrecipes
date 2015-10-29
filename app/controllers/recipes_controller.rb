71542130d43cc747cf1dae653ef11a45f132b50f
SaintKarene <stkarene@gmail.com>
2015-10-29 01:43:53 +0000

    add pagination

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,7 @@
 class RecipesController < ApplicationController
   
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.paginate(page: params[:page], per_page: 4) 
   end
   
   def show
@@ -28,10 +28,6 @@ class RecipesController < ApplicationController
     else render :new
     end
   end
-  private
-      def recipe_params
-        params.require(:recipe).permit(:name, :summary, :description, :picture)
-      end
   
   def edit
     @recipe = Recipe.find(params[:id])
@@ -46,9 +42,22 @@ class RecipesController < ApplicationController
     end
   end
   
-    
-      
-      
-      
+  def like
+    @recipe = Recipe.find(params[:id])
+    like = Like.create(like: (params[:like]), chef: Chef.first, recipe: @recipe)
+    if like.valid?
+    flash[:success] = "Your selection was successful!"
+    redirect_to :back
+  else
+    flash[:danger] = "Sorry. You can only vote for a recipe once."
+    redirect_to :back
+  end
+end
+  
+  private
+      def recipe_params
+        params.require(:recipe).permit(:name, :summary, :description, :picture)
+      end
+  
   
 end
